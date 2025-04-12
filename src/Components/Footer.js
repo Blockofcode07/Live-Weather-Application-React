5515759853e944ad10653ed4ffce18515e423466
Aditya <adityajawanjal27@gmail.com>
2025-04-13 03:49:10 +0530

    Added a neat footer with author name and version info for a polished finish

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Created by <strong>Aditya Jawanjal</strong> &nbsp;|&nbsp; Version: 0.1.0
+    </footer>
+  );
+};
+
+export default Footer;
